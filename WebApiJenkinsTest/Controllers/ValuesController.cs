073cb95f89ea4d486e697662998f23d88eaff288
David Mansilla <rdavidmansilla@gmail.com>
2019-11-21 14:35:48 -0600

    Pruebas de valroes

--- WebApiJenkinsTest/Controllers/ValuesController.cs
+++ WebApiJenkinsTest/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApiJenkinsTest.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "valu3" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
